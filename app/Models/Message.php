e1fb749d5319a6d9187b1f4fda3653cec466e127
IvaanOne <ivaanone17@gmail.com>
2022-08-04 13:44:33 +0200

    fixing Create Message v2

--- app/Models/Message.php
+++ app/Models/Message.php
@@ -8,7 +8,12 @@ use Illuminate\Database\Eloquent\Model;
 class Message extends Model
 {
     use HasFactory;
-
+    
+    protected $fillable = [
+        'content',
+        'channel_id',
+    ];
+    
     public function user()
     {
         return $this->belongsTo(User::class);
